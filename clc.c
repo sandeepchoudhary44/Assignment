62958a0789fcad96d8eeb0994a6071469e7620de
sandeepchoudhary44 <sandeep2808sirvi@gmail.com>
2026-02-03 20:56:09 +0530

    Add basic calculator functionality in clc.c
    Implement basic calculator operations including addition, subtraction, multiplication, and division.

---
+++ clc.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+float num1, num2, result;
+printf("enter the 2 number's:");
+printf("%f,%f",&num1,&num2);
+printf("addition : %f",num1+num2);
+printf("substraction : %f",num1-num2);
+printf("division : %f",num1/num2);
+printf("multiplication : %f",num1*num2);
+return 0;
+}
